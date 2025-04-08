6cf21b3935e9559a7e5fa6cf86cb499096c2cc3c
Zana Aziz <mail@zanaaziz.com>
2025-04-08 13:56:47 +0100

    enabled cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const { pool, initializeDatabase } = require('./config/db');
 require('dotenv').config();
 
 const app = express();
+
+app.use(cors());
 
 let port = process.env.PORT || 3000;
 
